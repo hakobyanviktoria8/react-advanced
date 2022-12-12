30c3dc8530a7951042ef09d9a18d037f1c78f688
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-12-12 22:43:09 +0400

    added point every button and change them every roll

--- src/components/games/Tenzies.js
+++ src/components/games/Tenzies.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./../../util/styles/Tenzies.css"
+import Point from './Point'
+
+
 
 function Tenzies() {
+  const [allPoints, setAllPoints] = useState(Array.from({length: 10}, () => Math.floor(Math.random() * 9) + 1))
+
+  function refreshPage() {
+    setAllPoints(Array.from({length: 10}, () => Math.floor(Math.random() * 9) + 1))
+    console.log(2222, allPoints);
+  }
+
   return (
-    <div>Tenzies</div>
+    <div className='Tenzies'>
+      <h2>Tenzies game</h2>
+      <div className='tenziesWrapper'>
+        <div className='tenziesPoints'>
+          {allPoints.map((item, idx) => <Point key={idx} item={item}/>)}
+        </div>
+        <button onClick={refreshPage}>Roll</button> 
+      </div>
+    </div>
   )
 }
 
