3557b6c586a40d8c14e6ab94d8bf744e3494e365
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-12-16 21:32:30 +0400

    use my custom hook

--- src/components/games/posts/Posts.jsx
+++ src/components/games/posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import { useFetching } from '../../../util/hooks/useFetching'
 import { Button } from '../../UI/button/Button'
 import styles from "./../../../util/styles/Posts.module.css"
 import { PostForm } from './PostForm'
@@ -11,16 +12,19 @@ export const Posts = () => {
   const [posts, setPosts] = useState(null)
   const [sortVal, setSortVal] = useState("")
   const [searchVal, setSearchVal] = useState("")
-  const [loading, setLoading] = useState(false)
+
+  const [fetchPosts, loading, postError] = useFetching(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    if(response.status === 200) {
+      const data = await response.json();
+      setPosts(data?.slice(0,10))
+    } else {
+      setPosts(null)
+    }
+  })
 
   useEffect(()=> {
-    setLoading(true)
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
-      setPosts(data?.slice(0,10));
-      setLoading(false)
-    })
+    fetchPosts()
   } ,[])
     
   const addPosts = (newPostItem) => {
@@ -36,22 +40,16 @@ export const Posts = () => {
     setPosts([...posts].sort((a, b) => a[sortVal]?.localeCompare(b[sortVal])))
   }
 
-  const handleSearchPost = async () => {
-    try {
-      setLoading(true)
-      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${searchVal}`);
-      if(response.status === 200) {
-        const data = await response.json();
-        setPosts([data])
-        setLoading(false)
-      } else {
-        setPosts(null)
-      }
-    } catch (error) {
-      console.log(error);
+  const [handleSearchPost] = useFetching(async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${searchVal}`)
+    if(response.status === 200) {
+      const data = await response.json();
+      setPosts([data])
+    } else {
+      setPosts(null)
     }
-  }
-
+  })
+  
   const handleChangeSearchId = (e) => {
     if(isNaN(e.target.value)) {
       return false
