d038a474050b0d0f45724ffb1a48fb447029879f
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-01-27 23:47:13 +0400

    added custom HOOK

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useInput } from '../../util/hooks/useInput';
 import "./../../util/styles/Footer.css"
 
-function Footer({mode}) {
+function Footer({ mode }) {
+  const [email, resetEmail] = useInput("");
+
+  const handleSubmit = (e) => {
+    // eslint-disable-next-line no-unused-expressions
+    e.preventDefault()
+    alert(email.val);
+    resetEmail();  
+  }
+
   return (
-    <div className={mode ? "dark footer" : "footer"}>Footer</div>
+    <div className={mode ? "dark footer" : "footer"}>
+      Footer
+      <form onSubmit={handleSubmit}>
+        <input
+          {...email}
+          type="text"
+          placeholder='Subscribe us...'
+        />
+        <button>Subscribe</button>
+      </form>
+    </div>
   )
 }
 
--- src/util/hooks/useFetching.js
+++ src/util/hooks/useFetching.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export const useFetching = (callback) => {
   const[loading, setLoading] = useState(false)
